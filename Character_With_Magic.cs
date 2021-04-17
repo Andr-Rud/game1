dc88d286386b4a1add38a0876978b49a60174117
Andrei Rudoi <79546228+Andr-Rud@users.noreply.github.com>
2021-04-17 13:22:37 +0300

    1 and 2 half

--- Character_With_Magic.cs
+++ Character_With_Magic.cs
@@ -7,13 +7,11 @@ namespace ClassCharacter
     {
         private int _mana_now;
         private int _mana_max;
-        private int _force;
         public Character_With_Magic(string name, Condition condition, Race race, bool ability_to_speak, bool ability_to_move, Gender gender, int age, int health_max, int health_now, int experience, int mana_max, int mana_now, int force)
             : base(name, condition, race, ability_to_speak, ability_to_move, gender, age, health_max, health_now, experience)
         {
             Set_Mana_Max(mana_max);
             Set_Mana_Now(mana_now);
-            Set_Force(force);
         }
         public void Set_Mana_Max(int mana_max)
         {
@@ -31,20 +29,13 @@ namespace ClassCharacter
         {
             return _mana_now;
         }
-        public int Get_Force(){
-            return _force;
-        }
-        public void Set_Force(int force)
-        {
-            _force = force;
-        }
         public override string ToString()
         {
-            return base.ToString() + " mana_max: " + _mana_max +'\n' + " mana_now: " + _mana_now + '\n';
+            return base.ToString() + " mana_max: " + _mana_max + '\n' + " mana_now: " + _mana_now + '\n';
         } 
-        public void Tell(Spell a,Character b)
+        public void Tell(Spell a, Character b)
         {
-            a.To_perform_a_magical_effect(b, _force);
+            a.To_perform_a_magical_effect(b, a.Get_Mana_Min());
         }
     }
 }
